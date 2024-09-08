787d89f9739cfd881598240fc8ec68be6c2d3406
alex <168668310+alxjann@users.noreply.github.com>
2024-09-08 16:53:49 +0800

    Create 1.cpp

---
+++ code/1.cpp
@@ -0,0 +1,35 @@
+/*
+AVERAGE OF 10 NUMBERS
+-Ask the user to enter 10 positive numbers
+-Display the 10 numbers and the average
+*/
+
+#include <iostream>
+#include <map> 
+
+using namespace std;
+
+int main()
+{
+    int sum, num;
+    map<int,int> numbers;
+
+    for (int i = 0; i < 10; i++) {
+        do {
+            cout << "[" << i+1 << "] " << "Enter positive number: ";
+            cin >> num;
+        } while(num < 0);
+        
+        sum += num;
+        numbers.insert({i+1,num});
+    }
+
+    
+    cout << "\nList of Numbers: \n";
+    
+    for(const auto& nums: numbers) {
+        cout << nums.first << ": " << nums.second << "\n\n";
+    }   
+    
+    cout << "Average of the numbers: " << sum / 10;
+}
